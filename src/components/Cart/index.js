638df80eb0e415c5b7588a64cfd584991b33b286
siva <nxtwave@ip-10-2-0-32.ap-south-1.compute.internal>
2023-12-15 04:42:15 +0000

    solution

---
+++ src/components/Cart/index.js
@@ -0,0 +1,21 @@
+// Write your JS code here
+
+import Header from '../Header'
+import './index.css'
+
+const Cart=()=>(
+    <>
+    <Header/>
+      <div className="cart-count">
+        <img 
+        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-cart-img.png "
+        alt="cart"
+        className="cart-img"
+        />
+      </div>
+
+    </>
+
+)
+
+export default Cart
---
+++ src/components/Products/index.js
@@ -0,0 +1,18 @@
+// Write your JS code here
+import Header from './Header'
+import './index.css'
+
+const Products = () => (
+  <>
+    <Header />
+    <div className="product-cont">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-img.png"
+        alt="products"
+        className="product-img"
+      />
+    </div>
+  </>
+)
+
+export default Products
